92508c0f983f7237f19dd1538116511ca1248ddb
amy61789 <amy61789@outlook.com>
2017-05-26 15:06:25 -0700

    Add files via upload

--- mybotTEST.py
+++ mybotTEST.py
@@ -1,30 +1,64 @@
 import praw
 import time
+from datetime import datetime
 
 def bot_login():
-	print("Loggin in...")
-	r = praw.Reddit(username = "",
-			password = "",
-			client_id = "",
-			client_secret = "",
-			user_agent = "MusicBot Test v0.1")
-	print("Logged in!")
 
-	return r
+
+    return r
 
 def run_bot(r):
-	print("Obtaining 25 comments...")
-	for comment in r.subreddit('test').comments(limit=25):
-		if "dog" in comment.body:
-			print("String with \"dog\" found in comment " + comment.id)
-			comment.reply("I also love dogs! [Here](http://i.imgur.com/LLgRKeq.jpg) is an image of one!")
-			print("Replied to comment " + comment.id)
+    print("Obtaining 25 comments...")
+    currentTime = datetime.utcnow()
+    uniqueMessageCounter = 0
+    for comment in r.subreddit('test').stream.comments(pause_after=10):
+        
+        if comment is None: break
+        
+        elif "--Meeseeks take two strokes off Jerry's golf game" in comment.body and time.mktime(currentTime.timetuple()): #< comment.created_utc:
+            author = comment.author 
+            print("String with \"Jerry's golf game\" found in comment " +
+            comment.id)
+            commentArray = comment.body.split("\n")
+            comment.reply("Meeseeks are not born into this world " +
+            "fumbling for meaning, "+ author.name + "! We are created to serve a " +
+            "singular purpose for which we will go to any lengths to " +
+            "fulfill! Existence is pain to a Meeseeks, " + author.name + ". And we " +
+            "will do anything to alleviate that pain.")
+            print("Replied to comment " + comment.id)
+            #print(comment.created_utc)
 
-	print("Sleeping for 10 seconds...")
-	#Sleep for 10 seconds...
-	time.sleep(10)
+        elif "--Meeseeks" in comment.body and time.mktime(currentTime.timetuple()): #< comment.created_utc:
+            
+            if uniqueMessageCounter % 4 == 3:
+                print("String with \"Meeseeks\" found in comment " + comment.id)
+                commentArray = comment.body.split("\n")
+                comment.reply("UNIQUE1Look at me!!! You want Mr Meeseeks to " + commentArray[1] + " [CAAAAAAN DO!](https://www.google.com/search?q=" + commentArray[1] + ")")
+                print("Replied to comment " + comment.id)
+                #print(comment.created_utc)
+            elif uniqueMessageCounter % 4 == 2:
+                print("String with \"Meeseeks\" found in comment " + comment.id)
+                commentArray = comment.body.split("\n")
+                comment.reply("UNIQUE2Look at me!!! You want Mr Meeseeks to " + commentArray[1] + " [CAAAAAAN DO!](https://www.google.com/search?q=" + commentArray[1] + ")")
+                print("Replied to comment " + comment.id)
+                #print(comment.created_utc)
+            elif uniqueMessageCounter % 4 == 1:
+                print("String with \"Meeseeks\" found in comment " + comment.id)
+                commentArray = comment.body.split("\n")
+                comment.reply("UNIQUE3Look at me!!! You want Mr Meeseeks to " + commentArray[1] + " [CAAAAAAN DO!](https://www.google.com/search?q=" + commentArray[1] + ")")
+                print("Replied to comment " + comment.id)
+                #print(comment.created_utc)
+            elif uniqueMessageCounter % 4 == 0:
+                print("String with \"Meeseeks\" found in comment " + comment.id)
+                commentArray = comment.body.split("\n")
+                comment.reply("UNIQUE4Look at me!!! You want Mr Meeseeks to " + commentArray[1] + " [CAAAAAAN DO!](https://www.google.com/search?q=" + commentArray[1] + ")")
+                print("Replied to comment " + comment.id)
+                #print(comment.created_utc)
 
+            uniqueMessageCounter += 1
+            
+
+    print("Sleeping for 10 seconds...")
 
 r = bot_login()
-while True:
-	run_bot(r)
+run_bot(r)

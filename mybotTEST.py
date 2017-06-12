01dc72624e748da11f317e2ac7e99bdf4b63d71a
amy61789 <amy61789@outlook.com>
2017-06-12 11:06:03 -0700

    Add files via upload

--- mybotTEST.py
+++ mybotTEST.py
@@ -3,57 +3,59 @@ import time
 from datetime import datetime
 
 def bot_login():
-
+    print("Logging in...")
+    r = praw.Reddit(username = "kimchiqween",
+            password = "Seattlerain17",
+            client_id = "f5hJLexO_19yuw",
+            client_secret = "_DdsEjIRsRGL0PyTT4cpmxrWnh0",
+            user_agent = "Meeseeks Test v0.1")
+    print("Logged in!")
 
     return r
 
 def run_bot(r):
     print("Obtaining 25 comments...")
-    currentTime = datetime.utcnow()
+
     uniqueMessageCounter = 0
     for comment in r.subreddit('test').stream.comments(pause_after=10):
         
+        commentArray = comment.body[11:]
+        
         if comment is None: break
         
-        elif "--Meeseeks take two strokes off Jerry's golf game" in comment.body and time.mktime(currentTime.timetuple()): #< comment.created_utc:
+        elif "--Meeseeks take two strokes off Jerry's golf game" in comment.body: 
             author = comment.author 
             print("String with \"Jerry's golf game\" found in comment " +
             comment.id)
-            commentArray = comment.body.split("\n")
             comment.reply("Meeseeks are not born into this world " +
             "fumbling for meaning, "+ author.name + "! We are created to serve a " +
             "singular purpose for which we will go to any lengths to " +
             "fulfill! Existence is pain to a Meeseeks, " + author.name + ". And we " +
             "will do anything to alleviate that pain.")
             print("Replied to comment " + comment.id)
-            #print(comment.created_utc)
 
-        elif "--Meeseeks" in comment.body and time.mktime(currentTime.timetuple()): #< comment.created_utc:
+        elif "--Meeseeks" in comment.body: 
             
             if uniqueMessageCounter % 4 == 3:
                 print("String with \"Meeseeks\" found in comment " + comment.id)
-                commentArray = comment.body.split("\n")
-                comment.reply("UNIQUE1Look at me!!! You want Mr Meeseeks to " + commentArray[1] + " [CAAAAAAN DO!](https://www.google.com/search?q=" + commentArray[1] + ")")
+                comment.reply("Look at me!!! You want Mr Meeseeks to " + commentArray + " [CAAAAAAN DO!](https://www.google.com/search?q=" + commentArray + ")")
                 print("Replied to comment " + comment.id)
-                #print(comment.created_utc)
+
             elif uniqueMessageCounter % 4 == 2:
                 print("String with \"Meeseeks\" found in comment " + comment.id)
-                commentArray = comment.body.split("\n")
-                comment.reply("UNIQUE2Look at me!!! You want Mr Meeseeks to " + commentArray[1] + " [CAAAAAAN DO!](https://www.google.com/search?q=" + commentArray[1] + ")")
+                comment.reply("Ooooooooooh! Caaaaaan do! You want Mr Meeseeks to " + commentArray + " [I'm Mr. Meeseeks, look at me!](https://www.google.com/search?q=" + commentArray + ")")
                 print("Replied to comment " + comment.id)
-                #print(comment.created_utc)
+
             elif uniqueMessageCounter % 4 == 1:
                 print("String with \"Meeseeks\" found in comment " + comment.id)
-                commentArray = comment.body.split("\n")
-                comment.reply("UNIQUE3Look at me!!! You want Mr Meeseeks to " + commentArray[1] + " [CAAAAAAN DO!](https://www.google.com/search?q=" + commentArray[1] + ")")
+                comment.reply("Meeseeks don't usually have to exist for this long. It's gettin' weeeiiird..." + commentArray + " [CAAAAAAN DO!](https://www.google.com/search?q=" + commentArray + ")")
                 print("Replied to comment " + comment.id)
-                #print(comment.created_utc)
+
             elif uniqueMessageCounter % 4 == 0:
                 print("String with \"Meeseeks\" found in comment " + comment.id)
-                commentArray = comment.body.split("\n")
-                comment.reply("UNIQUE4Look at me!!! You want Mr Meeseeks to " + commentArray[1] + " [CAAAAAAN DO!](https://www.google.com/search?q=" + commentArray[1] + ")")
+                comment.reply("Meeseeks are created to serve a singular purpose, for which we will go to any lengths to fulfill!" + commentArray + " [CAAAAAAN DO!](https://www.google.com/search?q=" + commentArray + ")")
                 print("Replied to comment " + comment.id)
-                #print(comment.created_utc)
+
 
             uniqueMessageCounter += 1
             
